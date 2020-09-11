cfeef919af3006ce2090f77390c694fcd9b7009a
andrehgdias <andredias.us@gmail.com.com>
2020-09-11 16:05:06 -0300

    feat: Logica de login e logout adicionada, navbar sendo alterada dinâmicamente

---
+++ js/main.js
@@ -0,0 +1,91 @@
+window.addEventListener("load", async function () {
+	let innerHtmlButtons = window.localStorage.getItem("user")
+		? "<li><button class='custom-button' id='sair'>Sair</button>"
+		: "<li><button class='custom-button' id='entrar'>Entrar</button></li><li><button class='custom-button' id='cadastrar'>Cadastrar</button></li>";
+	document.getElementById("buttons").innerHTML = innerHtmlButtons;
+
+	toggleSidebar = () => {
+		document.getElementsByClassName("mask")[0].classList.toggle("hide");
+		document.getElementById("navbar").classList.toggle("sidebar");
+	};
+
+	animModal = (modal, animType) => {
+		let modalElement = document.getElementById(modal);
+		if (animType === "open") modalElement.classList.add("showModalAnim");
+		else modalElement.classList.remove("showModalAnim");
+	};
+
+	toggleModal = (modal) => {
+		document.getElementById(modal).classList.toggle("hide");
+	};
+
+	favorite = (e) => {
+		e.target.classList.toggle("star-selected");
+	};
+
+	like = (e) => {
+		e.target.classList.toggle("thumbs-selected");
+	};
+
+	var mySwiper = new Swiper(".swiper-container", {
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		slidesPerView: 1,
+		breakpoints: {
+			768: {
+				slidesPerView: 2,
+			},
+			1024: {
+				slidesPerView: 3,
+			},
+		},
+	});
+
+	document
+		.getElementsByClassName("mask")[0]
+		.addEventListener("click", toggleSidebar);
+	document
+		.getElementsByClassName("sm-only")[0]
+		.addEventListener("click", toggleSidebar);
+
+	let stars = document.getElementsByClassName("star");
+	for (var i = 0; i < stars.length; i++) {
+		stars[i].addEventListener("click", favorite);
+	}
+
+	let thumbs = document.getElementsByClassName("thumbs");
+	for (var i = 0; i < thumbs.length; i++) {
+		thumbs[i].addEventListener("click", like);
+	}
+
+	let comments = document.getElementsByClassName("comment");
+	for (var i = 0; i < comments.length; i++) {
+		comments[i].addEventListener("click", () => {
+			animModal("modalComments", "open");
+			toggleModal("modalComments");
+		});
+	}
+
+	// Checking id api is online
+	try {
+		let response = await fetch(
+			"https://rotten-potatos-api.herokuapp.com/ping"
+		);
+		let data = await response.text();
+		console.info(data);
+	} catch (error) {
+		console.warn(error);
+		console.warn("HTTP-Error: " + response.status);
+	}
+});
+
+function sleep(ms) {
+	console.info("Sleep: ", ms );
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
---
+++ js/modais.js
@@ -0,0 +1,172 @@
+window.addEventListener("load", async function () {
+	if (window.localStorage.getItem("user")) {
+		document.getElementById("sair").addEventListener("click", async () => {
+			console.log("Log out...");
+			window.localStorage.removeItem("user");
+			Swal.fire({
+				toast: true,
+				position: "top",
+				showConfirmButton: false,
+				title: "Sucesso",
+				text: `LOGOUT realizado com sucesso, você será redirecionado automaticamente!`,
+				type: "success",
+			});
+			await sleep(1500);
+			history.go(0);
+		});
+		document
+			.getElementById("closeModalCommentsButton")
+			.addEventListener("click", () => {
+				Swal.fire({
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					title: "Você não está logado!",
+					text: "Faça login para poder adicionar um comentário!",
+					type: "info",
+				});
+			});
+	} else {
+		document.getElementById("entrar").addEventListener("click", () => {
+			animModal("modalLogin", "open");
+			toggleModal("modalLogin");
+		});
+
+		document
+			.getElementById("closeModalLoginButton")
+			.addEventListener("click", () => {
+				animModal("modalLogin", "close");
+				toggleModal("modalLogin");
+			});
+
+		document.getElementById("cadastrar").addEventListener("click", () => {
+			animModal("modalSignup", "open");
+			toggleModal("modalSignup");
+		});
+
+		document
+			.getElementById("closeModalSignupButton")
+			.addEventListener("click", () => {
+				animModal("modalSignup", "close");
+				toggleModal("modalSignup");
+			});
+
+		document
+			.getElementById("closeModalCommentsButton")
+			.addEventListener("click", () => {
+				animModal("modalComments", "close");
+				toggleModal("modalComments");
+			});
+	}
+
+	document
+		.getElementById("formLogin")
+		.addEventListener("submit", async (event) => {
+			event.preventDefault();
+
+			let object = {};
+			for (let index = 0; index < 2; index++) {
+				object[event.target[index].id] = event.target[index].value;
+			}
+			let json = JSON.stringify(object);
+			console.log(json);
+
+			const options = {
+				method: "POST",
+				body: json,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			};
+
+			try {
+				console.clear();
+				let response = await fetch(
+					"https://rotten-potatos-api.herokuapp.com/login",
+					options
+				);
+
+				let json = await response.json();
+				window.localStorage.setItem("user", JSON.stringify(json));
+				Swal.fire({
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					title: "Sucesso",
+					text: `LOGIN realizado com sucesso, você será redirecionado automaticamente!`,
+					type: "success",
+				});
+				await sleep(1500);
+				animModal("modalLogin", "close");
+				toggleModal("modalLogin");
+				history.go(0);
+			} catch (error) {
+				Swal.fire({
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					title: "Erro durante o login!",
+					text:
+						"Houve algum problema com seu login, verifique suas informações e tente novamente!",
+					type: "error",
+				});
+				console.warn(error);
+			}
+		});
+
+	document
+		.getElementById("formSignup")
+		.addEventListener("submit", async (event) => {
+			event.preventDefault();
+
+			let object = {};
+			for (let index = 0; index < 4; index++) {
+				object[event.target[index].id] = event.target[index].value;
+			}
+			let json = JSON.stringify(object);
+			console.log(json);
+
+			const options = {
+				method: "POST",
+				body: json,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			};
+
+			try {
+				console.clear();
+				let response = await fetch(
+					"https://rotten-potatos-api.herokuapp.com/cadastro",
+					options
+				);
+
+				let text = await response.text();
+
+				if (text == "Salvo") {
+					Swal.fire({
+						toast: true,
+						position: "top",
+						showConfirmButton: false,
+						title: "Sucesso",
+						text: `Cadastro realizado com sucesso, faça seu login!`,
+						type: "success",
+					});
+					animModal("modalSignup", "close");
+					toggleModal("modalSignup");
+				} else {
+					Swal.fire({
+						toast: true,
+						position: "top",
+						showConfirmButton: false,
+						title: "Erro durante o cadastro!",
+						text:
+							"Houve algum problema com seu cadastro, verifique suas informações e tente novamente!",
+						type: "error",
+					});
+				}
+			} catch (error) {
+				console.warn(error);
+			}
+		});
+});
